a27a8a09a9b6329249444a081bb7834e471f79e1
ReyRachel8L <rlim.rachel08@gmail.com>
2022-06-23 14:29:39 +0800

    Initial Commit

---
+++ SAP project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SAP project
+//
+//  Created by LIM YEE SIN RACHEL stu on 23/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SAP project/SAP_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP_projectApp.swift
+//  SAP project
+//
+//  Created by LIM YEE SIN RACHEL stu on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SAP_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
